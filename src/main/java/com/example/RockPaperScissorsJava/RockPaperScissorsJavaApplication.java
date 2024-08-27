41af3a7060fe2379c946add37c3f2bcabf134d04
Austin Woods <austinwoods99@gmail.com>
2024-08-26 20:37:11 -0500

    Initial Commit

---
+++ src/main/java/com/example/RockPaperScissorsJava/RockPaperScissorsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.RockPaperScissorsJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockPaperScissorsJavaApplication.class, args);
+	}
+
+}
